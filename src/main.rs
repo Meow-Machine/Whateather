8f238521197289d8a3304ef1959f05b3f122ff49
meow-machine-tower <DavidGrochowicki@gmx.de>
2025-02-28 00:55:43 +0100

    Weather and Location Fetcher done

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,26 @@
-use reqwest::{Client, Response};
-use crate::weather::MeteoWeatherResponse;
 use tokio::runtime::Runtime;
+use crate::weather::{WeatherFetcher};
 
 mod cache;
 mod weather;
 
-mod util;
 
 const CACHE_FILE_PATH: &str = "./cache.txt";
 
-
 fn main () {
-    try_cache()
-}
 
-fn try_fetch() {
-    let latitude = 52.279911;
-    let longitude = 8.0471719;
+    let weather_fetcher = WeatherFetcher::new();
 
-    let url = format!(
-        "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current=temperature_2m",
-        latitude,
-        longitude
-    );
-    let client = Client::new();
+    let data = Runtime::new().expect("Failed to create Runtime")
+        .block_on(weather_fetcher.fetch_data());
 
-    let response = get_new_data(&client, &*url);
-}
-fn get_new_data(client: &Client, url: &str) {
+    match data {
+        Ok(data) => {
+            println!("{:?}", data)
+        },
+        Err(err) => {
+            eprintln!("{:?}", err)
+        }
+    }
 
-    let runtime = Runtime::new().unwrap();
-
-    let response = runtime.block_on(async {
-        let response = util::network::retrying_get(client, url)
-            .await
-            .unwrap();
-
-        response.json::<MeteoWeatherResponse>()
-            .await
-            .unwrap()
-    });
-
-    println!("{:#?}", response);
-}
-
-fn try_cache() {
-    let mut cache = cache::Cache::create(CACHE_FILE_PATH);
-    cache.read_cache_file();
-    cache.print_data();
-
-    cache.write_data_to_cache(b"HalloWeltchen");
-    cache.print_data();
 }
--- src/weather/mod.rs
+++ src/weather/mod.rs
@@ -1,29 +1,112 @@
+use reqwest::Client;
 use serde::Deserialize;
 
-/// JSON response format from open-meteo
-#[derive(Deserialize, Debug)]
-pub struct MeteoWeatherResponse {
-    pub current_units: UnitData,
-    pub current: CurrentData,
+mod meteo {
+    use serde::Deserialize;
+    use crate::weather::{TemperatureUnit, UniversalTemperatureData};
+
+    /// JSON response format from open-meteo
+    #[derive(Deserialize, Debug)]
+    pub struct MeteoWeatherResponse {
+        current_units: UnitData,
+        current: CurrentData,
+    }
+
+    #[derive(Deserialize, Debug)]
+    pub struct CurrentData {
+        temperature_2m: Option<f32>,
+    }
+
+    #[derive(Deserialize, Debug)]
+    pub struct UnitData {
+        temperature_2m: Option<String>,
+    }
+
+    impl MeteoWeatherResponse {
+        pub fn into_universal(self) -> Result<UniversalTemperatureData,String> {
+
+            let unit = self.current_units.into_temperature_unit()?;
+            let temperature = self.current.into_f32()?;
+
+            Ok(UniversalTemperatureData::new(temperature, unit))
+        }
+    }
+
+    impl UnitData {
+        pub fn into_temperature_unit(self) -> Result<TemperatureUnit, String> {
+            let unit = self.temperature_2m.as_ref().ok_or_else(|| "Meteo did not Provide Unit-Data".to_string())?.as_str();
+            Self::convert_unit(unit)
+        }
+        fn convert_unit(unit: &str) -> Result<TemperatureUnit, String> {
+            match unit {
+                "°C" => Ok(TemperatureUnit::Celsius),
+                _ => {Err(format!("Unit translation not possible for {}", unit))}
+            }
+        }
+
+    }
+    impl CurrentData {
+        pub fn into_f32(self) -> Result<f32, String> {
+            self.temperature_2m.ok_or_else(|| "Meteo did not Provide Temperature-Data".to_string())
+        }
+    }
 }
 
 #[derive(Deserialize, Debug)]
-pub struct CurrentData {
-    pub temperature_2m: Option<f32>,
+pub struct GeoLocation {
+    pub lat: f32,
+    pub lon: f32,
+}
+#[derive(Debug)]
+pub struct UniversalTemperatureData {
+    temperature: f32,
+    unit: TemperatureUnit,
+}
+#[derive(Debug)]
+pub enum TemperatureUnit {
+    Celsius,
+    Kelvin,
+    Fahrenheit,
 }
 
-#[derive(Deserialize, Debug)]
-pub struct UnitData {
-    pub temperature_2m: Option<String>,
+pub struct WeatherFetcher {
+    client: Client,
 }
 
+impl WeatherFetcher {
+    pub fn new() -> Self { WeatherFetcher { client: Client::new(), } }
 
-// pub struct UniversalTemperatureData {
-//     temperature: f32,
-//     unit: TemperatureUnit,
-// }
-// pub enum TemperatureUnit {
-//     Celsius,
-//     Kelvin,
-//     Fahrenheit,
-// }
+    pub async fn fetch_data(&self)-> Result<UniversalTemperatureData, Box<dyn std::error::Error>> {
+        let geolocation = self.fetch_location().await?;
+        let meteo_data = self.fetch_weather(geolocation).await?;
+
+        meteo_data.into_universal().map_err(|e| e.into())
+    }
+
+    async fn fetch_weather(&self, geo_location: GeoLocation) -> Result<meteo::MeteoWeatherResponse, reqwest::Error> {
+        let url = format!(
+            "https://api.open-meteo.com/v1/forecast?latitude={}&longitude={}&current=temperature_2m",
+            geo_location.lat, geo_location.lon,
+        );
+
+        let response = self.client.get(url).send().await?;
+        let json = response.json::<meteo::MeteoWeatherResponse>().await?;
+
+        Ok(json)
+    }
+
+    async fn fetch_location(&self) -> Result<GeoLocation, reqwest::Error> {
+        let url = "http://ip-api.com/json/";
+        let response = self.client.get(url).send().await?;
+        let json = response.json::<GeoLocation>().await?;
+        Ok(json)
+    }
+
+
+}
+
+impl UniversalTemperatureData {
+    pub fn new(temperature: f32, unit: TemperatureUnit) -> Self {
+        Self { temperature, unit }
+    }
+}
